db884b49b89ee7dba5cf79a5c5961e0ae56d4b5c
hachi-ops <elen20@protonmail.com>
2021-07-11 00:43:42 +0100

    create components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// import logo from "./logo.svg";
-// import "./App.css";
 import React from "react";
 
 function App() {
@@ -7,25 +5,75 @@ function App() {
     <div>
       <Logo/>
       <BestPokemon/>
+      <CaughtPokemon/>
     </div>
   );
 }
 
-function Logo() {
+// function Logo() {
+//   const appName = "Hachi's Pokemon";
+//   return (
+//     <div>
+//       <header>
+//       <h1>Welcome to {appName}</h1>
+//     <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="" />
+//     </header>
+//     </div>
+//   );
+// }
+
+const Logo = () => {
+  const appName = "Helen's Pokemon";
+return (
+<div>
+<header>
+<h1>Welcome to {appName}</h1>
+<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="" />
+</header>
+</div>
+);
+};
+
+const CaughtPokemon = () => {
+  const date = new Date().toLocaleDateString();
+   return (
+    <p>
+  caught 0 pokemon on {date}</p>
+  );
+
+};
+
+// const Logo = () => (
+//   <div>
+//   <header>
+//   <h1>Welcome to the Pokedex</h1>
+//   <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="" />
+//   </header>
+//   </div>
+// );
+
+// function BestPokemon() {
+//   return (
+//    <p>My favourite pokemon is not Pidgey</p>
+//   );
+// }
+
+const BestPokemon = () => {
+  const abilities = ["Anticipation", "Adaptability", "Run-Away"];
   return (
     <div>
-      <header>
-      <h1>Welcome to the Pokedex</h1>
-    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="" />
-    </header>
+<p>My favourite pokemon is not Pidgey</p>
+<uL>
+  {abilities.map((name, index) => (
+    <li key = {index} > {name}</li>
+  ))}
+</uL>
     </div>
   );
-}
+};
 
-function BestPokemon() {
-  return (
-   <p>My favourite pokemon is Pidgey</p>
-  );
-}
+// const BestPokemon = () => (
+// <p>My favourite pokemon is not Pidgey</p>
+// );
 
 export default App;
